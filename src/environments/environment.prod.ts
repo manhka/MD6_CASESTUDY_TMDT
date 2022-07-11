e6b92b4a9779e3a1956254402dee6b93b34d8da4
unknown <manh87835@gmail.com>
2022-07-11 16:14:08 +0700

    manh lan 1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+    apiKey: "AIzaSyBn0u4LqsXpfEwZXgArGx-p5TOsQUIq2IM",
+    authDomain: "blog-firebase-c1eff.firebaseapp.com",
+    projectId: "blog-firebase-c1eff",
+    storageBucket: "blog-firebase-c1eff.appspot.com",
+    messagingSenderId: "350356222509",
+    appId: "1:350356222509:web:f734799b0674c639b67ea4",
+    measurementId: "G-6X33ESYR8P"
+  },
+  production: true
+};
